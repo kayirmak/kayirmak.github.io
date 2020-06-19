09b4929b1659ac0296e6660d88b0ea27bc932d43
kayirmak <dzhumabaev.kai@gmail.com>
2020-06-19 18:33:55 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,22 @@
+$(document).ready(() =>{
+
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        dots: true,
+        responsive:{
+            0:{
+                items:1,
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5,
+            }
+        }
+    })
+    
+})
